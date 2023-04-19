f3e06f9dd8a6783ded90390b1d81891e57556c85
Roman Boyko <112849174+RomanVillain@users.noreply.github.com>
2023-04-19 10:44:01 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,49 @@
 import telegram
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import requests
+from telegram import KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 
 BOT_TOKEN = "6146695182:AAGpz2yFs_9U-3hARDYGEeXsaCpzaicEuEQ"
 
-# Создаем экземпляр Updater и Dispatcher
+# Створюємо екземпляр Updater і Dispatcher
 updater = Updater(token=BOT_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 def send_message_with_buttons(update, context):
-    # Создаем кнопки
-    keyboard = [[InlineKeyboardButton("Кнопка 1", callback_data='button1'),
-                 InlineKeyboardButton("Кнопка 2", callback_data='button2'),
-                 InlineKeyboardButton("Кнопка 3", callback_data='button3')]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    # Отправляем сообщение с кнопками
+    # Створюємо кнопки
+    keyboard = [[KeyboardButton("Народні", callback_data='button1'), KeyboardButton("Етно-рок", callback_data='button2'),
+                 KeyboardButton("Популярні", callback_data='button3'), KeyboardButton("Християнські", callback_data='button4')]]
+    reply_markup = ReplyKeyboardMarkup(keyboard)
+    # Відправляємо повідомлення з кнопками
     context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Виберіть закладку:",
+                             text="Вибери тип пісень, які ти хочеш послухати:",
+                            
                              reply_markup=reply_markup)
 
 def button_click(update, context):
     query = update.callback_query
     if query.data == 'button1':
-        # Обработка нажатия кнопки 1
+        # Обробка натискання кнопки 1
         pass
     elif query.data == 'button2':
-        # Обработка нажатия кнопки 2
+        # Обробка натискання кнопки 2
+        pass
+    elif query.data == 'button3':
+        # Обробка натискання кнопки 3
+        pass
+    elif query.data == 'button4':
+        # Обробка натискання кнопки 4
         pass
 
-# Регистрируем обработчик для команды /start
+
+
+# Реєструємо оброблювач для команди /start
 start_handler = CommandHandler('start', send_message_with_buttons)
 dispatcher.add_handler(start_handler)
 
-# Регистрируем обработчик для нажатий на кнопки
+# Реєструємо обробник для натискань на кнопки
 button_handler = CallbackQueryHandler(button_click)
 dispatcher.add_handler(button_handler)
 
-# Запускаем бота
-updater.start_polling()
+# Запускаєм бота
+updater.start_polling() 
